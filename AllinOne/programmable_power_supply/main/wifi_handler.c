5080370e54f2e4622216de150376c014fd4835dc
Abhishektiwari7 <abhishektiwari939@gmail.com>
2023-03-21 23:04:31 +0530

    HTML code updated, get string of data from user, store data in esp32, css pending, rotary encoder upcoming

--- AllinOne/programmable_power_supply/main/wifi_handler.c
+++ AllinOne/programmable_power_supply/main/wifi_handler.c
@@ -5,10 +5,11 @@
 //-------data stream html----------------------------------------
 float vol,curr;
 uint16_t rawadc;
+//https://somsakelect.com/converter/text-to-string-converter.html
 char html_page[] = "<!DOCTYPE HTML><html>\n"
                    "<head>\n"
                    "  <title>ESP-IDF Programmable Power Supply Web Server</title>\n"
-                   "  <meta http-equiv=\"refresh\" content=\"5\">\n"
+                  // "  <meta http-equiv=\"refresh\" content=\"5\">\n"
                    "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n"
                    "  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\" integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">\n"
                    "  <link rel=\"icon\" href=\"data:,\">\n"
@@ -44,16 +45,24 @@ char html_page[] = "<!DOCTYPE HTML><html>\n"
                    "      </div>\n"
                    "    </div>\n"
                    "  </div>\n"
+                   " <form action=\"/get\">\n"
+                   " Set Voltage: <input type=\"text\" name=\"v_\">\n"
+                   " <input type=\"submit\" value=\"Submit\">\n"
+                   "</form><br>\n"
+                   " <form action=\"/get\">\n"
+                   " Set Current: <input type=\"text\" name=\"a_\">\n"
+                   " <input type=\"submit\" value=\"Submit\">\n"
+                   "</form><br>\n"
                    "</body>\n"
                    "</html>";
 /////////////////////////////////////////////////////////////////
 
-char on_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> ON</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
+// char on_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\" ></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> ON</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
 
-char off_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\"></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> OFF</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
+// char off_resp[] = "<!DOCTYPE html><html><head><style type=\"text/css\">html {  font-family: Arial;  display: inline-block;  margin: 0px auto;  text-align: center;}h1{  color: #070812;  padding: 2vh;}.button {  display: inline-block;  background-color: #b30000; //red color  border: none;  border-radius: 4px;  color: white;  padding: 16px 40px;  text-decoration: none;  font-size: 30px;  margin: 2px;  cursor: pointer;}.button2 {  background-color: #364cf4; //blue color}.content {   padding: 50px;}.card-grid {  max-width: 800px;  margin: 0 auto;  display: grid;  grid-gap: 2rem;  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));}.card {  background-color: white;  box-shadow: 2px 2px 12px 1px rgba(140,140,140,.5);}.card-title {  font-size: 1.2rem;  font-weight: bold;  color: #034078}</style>  <title>ESP32 WEB SERVER</title>  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">  <link rel=\"icon\" href=\"data:,\">  <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\"    integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">  <link rel=\"stylesheet\" type=\"text/css\"></head><body>  <h2>ESP32 WEB SERVER</h2>  <div class=\"content\">    <div class=\"card-grid\">      <div class=\"card\">        <p><i class=\"fas fa-lightbulb fa-2x\" style=\"color:#c81919;\"></i>     <strong>GPIO2</strong></p>        <p>GPIO state: <strong> OFF</strong></p>        <p>          <a href=\"/led2on\"><button class=\"button\">ON</button></a>          <a href=\"/led2off\"><button class=\"button button2\">OFF</button></a>        </p>      </div>    </div>  </div></body></html>";
 
 static const char *TAG = "espressif"; // TAG for debug
-int led_state = 0;
+// int led_state = 0;
 
 #define EXAMPLE_ESP_WIFI_SSID "Itsmine"
 #define EXAMPLE_ESP_WIFI_PASS "SG3524INV"
@@ -191,18 +200,91 @@ esp_err_t get_req_handler(httpd_req_t *req)
     return send_web_page(req);
 }
 
-esp_err_t led_on_handler(httpd_req_t *req)
+// esp_err_t led_on_handler(httpd_req_t *req)
+// {
+//     gpio_set_level(LED_PIN, 1);
+//     led_state = 1;
+//     return send_web_page(req);
+// }
+
+// esp_err_t led_off_handler(httpd_req_t *req)
+// {
+//     gpio_set_level(LED_PIN, 0);
+//     led_state = 0;
+//     return send_web_page(req);
+// }
+
+esp_err_t get_handler_str(httpd_req_t *req) //send value ipaddress/get?str=f or ipaddress/get?int=1233 
 {
-    gpio_set_level(LED_PIN, 1);
-    led_state = 1;
-    return send_web_page(req);
+    // Read the URI line and get the host
+    char *buf;
+    size_t buf_len;
+    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
+    if (buf_len > 1)
+    {
+        buf = malloc(buf_len);
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK)
+        {
+            ESP_LOGI(TAG, "Host: %s", buf);
+        }
+        free(buf);
+    }
+
+    // Read the URI line and get the parameters
+     buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found URL query: %s", buf);
+            char param[32];
+            if (httpd_query_key_value(buf, "v_", param, sizeof(param)) == ESP_OK) {  //str is key word as field
+                ESP_LOGI(TAG, "The Voltage value = %s", param);
+            }
+            if (httpd_query_key_value(buf, "a_", param, sizeof(param)) == ESP_OK) { //int is key word as field
+                ESP_LOGI(TAG, "The Current value = %s", param);
+            }
+        }
+        free(buf);
+    }
+
+    // // The response
+    // const char resp[] = "The data was sent ...";
+    // httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    // return ESP_OK;
+    return send_web_page(req); //return to main
 }
 
-esp_err_t led_off_handler(httpd_req_t *req)
+esp_err_t post_handler(httpd_req_t *req)
 {
-    gpio_set_level(LED_PIN, 0);
-    led_state = 0;
-    return send_web_page(req);
+    /* Destination buffer for content of HTTP POST request.
+     * httpd_req_recv() accepts char* only, but content could
+     * as well be any binary data (needs type casting).
+     * In case of string data, null termination will be absent, and
+     * content length would give length of string */
+    char content[100];
+
+    /* Truncate if content length larger than the buffer */
+    //size_t recv_size = MIN(req->content_len, sizeof(content));
+
+    size_t recv_size = sizeof(content);
+    int ret = httpd_req_recv(req, content, recv_size);
+    if (ret <= 0) {  /* 0 return value indicates connection closed */
+        /* Check if timeout occurred */
+        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+            /* In case of timeout one can choose to retry calling
+             * httpd_req_recv(), but to keep it simple, here we
+             * respond with an HTTP 408 (Request Timeout) error */
+            httpd_resp_send_408(req);
+        }
+        /* In case of error, returning ESP_FAIL will
+         * ensure that the underlying socket is closed */
+        return ESP_FAIL;
+    }
+
+    /* Send a simple response */
+    const char resp[] = "URI POST Response";
+    httpd_resp_send(req, resp, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
 }
 
 httpd_uri_t uri_get = {
@@ -211,17 +293,31 @@ httpd_uri_t uri_get = {
     .handler = get_req_handler,
     .user_ctx = NULL};
 
-httpd_uri_t uri_on = {
-    .uri = "/led2on",
+// httpd_uri_t uri_on = {
+//     .uri = "/led2on",
+//     .method = HTTP_GET,
+//     .handler = led_on_handler,
+//     .user_ctx = NULL};
+
+// httpd_uri_t uri_off = {
+//     .uri = "/led2off",
+//     .method = HTTP_GET,
+//     .handler = led_off_handler,
+//     .user_ctx = NULL};
+
+httpd_uri_t uri_get_input = { //get data from html page
+    .uri = "/get",
     .method = HTTP_GET,
-    .handler = led_on_handler,
+    .handler = get_handler_str,
     .user_ctx = NULL};
 
-httpd_uri_t uri_off = {
-    .uri = "/led2off",
-    .method = HTTP_GET,
-    .handler = led_off_handler,
-    .user_ctx = NULL};
+/* URI handler structure for POST /uri */
+httpd_uri_t uri_post = {
+    .uri      = "/uri",
+    .method   = HTTP_POST,
+    .handler  = post_handler,
+    .user_ctx = NULL
+};
 
 httpd_handle_t setup_server(void)
 {
@@ -231,8 +327,10 @@ httpd_handle_t setup_server(void)
     if (httpd_start(&server, &config) == ESP_OK)
     {
         httpd_register_uri_handler(server, &uri_get);
-        httpd_register_uri_handler(server, &uri_on);
-        httpd_register_uri_handler(server, &uri_off);
+        // httpd_register_uri_handler(server, &uri_on);
+        // httpd_register_uri_handler(server, &uri_off);
+        httpd_register_uri_handler(server, &uri_get_input);
+        httpd_register_uri_handler(server, &uri_post);
     }
 
     return server;
@@ -255,7 +353,7 @@ void initWifi() {
    // gpio_pad_select_gpio(LED_PIN);
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
 
-    led_state = 0;
+    //led_state = 0;
     ESP_LOGI(TAG, "LED Control Web Server is running ... ...\n");
     setup_server();
 }
--- AllinOne/programmable_power_supply/main/wifi_handler.h
+++ AllinOne/programmable_power_supply/main/wifi_handler.h
@@ -15,7 +15,7 @@ extern "C" {
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "spi_flash_mmap.h" //#include "esp_spi_flash.h" //depricated
 #include <esp_http_server.h>
 
 #include "esp_wifi.h"
